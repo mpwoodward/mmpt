a34d4294604b62a1c60dd4b522c68cd74620aa46
Matthew Woodward <matthew_woodward@saa.senate.gov>
2019-02-09 16:59:28 -0800

    added select2 for categories

--- films/admin.py
+++ films/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Category, Director, Distributor, Film
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    search_fields = ['name', ]
+
+
 class DirectorAdmin(admin.ModelAdmin):
     model = Director
     list_display = ['last_name', 'first_name', ]
@@ -17,10 +22,10 @@ class DistributorAdmin(admin.ModelAdmin):
 class FilmAdmin(admin.ModelAdmin):
     model = Film
     list_display = ['title', 'release_year', ]
-    autocomplete_fields = ['directors', 'distributor', ]
+    autocomplete_fields = ['categories', 'directors', 'distributor', ]
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Director, DirectorAdmin)
 admin.site.register(Distributor, DistributorAdmin)
 admin.site.register(Film, FilmAdmin)
